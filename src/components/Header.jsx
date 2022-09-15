0fa33dc67b4bdc7e5c6fd89cb335bdb8c087c8fa
AngelJimenezC <105087736+AngelJimenezC@users.noreply.github.com>
2022-09-14 21:41:54 -0500

    segundo dia

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,30 @@
-
+import Button from "./Button"
 
 const Header = ({titulo}) => {
+
+const onClick = () =>{
+  console.log("click");
+}
+
+
+
   return (
-    <header>
+    <header className="header">
         <h1>{titulo}</h1>
+        <Button texto="agregar" color="black" onClick={onClick}/>
+       
     </header>
   )
 }
+
+
+Header.defaultProps = {
+  titulo: 'Tareas'
+}
+
+/* const estilo = {
+  color:"red"
+  
+} */
 
 export default Header
